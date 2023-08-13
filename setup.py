5032fa78ad732955130f2d05e18f7cda0d94428c
tav33 <tav@campus.technion.ac.il>
2023-08-13 14:35:06 +0300

    .

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(
     name="improved_diffusion_for_SMLM",
     py_modules=["improved_diffusion_for_SMLM"],
     install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
+    download_url= 'https://github.com/tavnah/improved_diffusion_for_SMLM/archive/refs/tags/v_01.tar.gz',
 )
