457a731813000017d573c70c0ed9ec0e3f191c52
tav33 <tav@campus.technion.ac.il>
2023-09-25 11:31:08 +0300

    new vesrion

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     packages=find_packages(),
     install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
     download_url= 'https://github.com/tavnah/improved_diffusion_for_SMLM/archive/refs/tags/v_01.tar.gz',
-    version="0.1.3.6",
+    version="0.1.3.8",
 )
